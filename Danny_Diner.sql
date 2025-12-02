131dd51c1adb83464a341866f8563100fe9a4f01
jakharaz <akharazjihane@gmail.com>
2025-12-02 20:14:16 +0100

    What was the first item from the menu purchased by each customer?

--- Danny_Diner.sql
+++ Danny_Diner.sql
@@ -67,3 +67,24 @@ customer_id,
 COUNT(DISTINCT order_date) as days
 From dannys_diner.sales
 GROUP BY customer_id;
+
+#3.What was the first item from the menu purchased by each customer?
+WITH ordered_sales AS (
+  SELECT
+  sales.customer_id,
+  sales.order_date,
+  menu.product_name,
+  DENSE_RANK() OVER (
+    PARTITION BY sales.customer_id
+    ORDER BY sales.order_date
+  ) AS rank
+  FROM dannys_diner.sales
+  JOIN dannys_diner.menu
+  ON sales.product_id = menu.product_id
+)
+SELECT
+customer_id,
+product_name
+FROM ordered_sales
+WHERE rank = 1
+GROUP BY customer_id, product_name;
